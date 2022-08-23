f8eb372e7a0e47cfecfddbf303d0735264fbfc6c
Travis Hawley <travis.hawley@ibotta.com>
2022-08-23 12:30:07 -0700

    updated config

--- terraform/checkov/custom-policies/aws/APIGatewayLogging.py
+++ terraform/checkov/custom-policies/aws/APIGatewayLogging.py
@@ -1,28 +1,34 @@
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.common.models.enums import CheckResult, CheckCategories
+# from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+# from checkov.common.models.enums import CheckResult, CheckCategories
+# from lark import Token
 
 
-class APIGatewayLogging(BaseResourceCheck):
-    def __init__(self):
-        name = "Ensure RDS instances are configured for at least 3 AZs.  Also add at least one reader node for Aurora clusters."
-        id = "CKV_IBT_010"
-        supported_resources = ['aws_api_gateway_method_settings']
-        # Look at checkov/common/models/enums.py for options
-        categories = [CheckCategories.BACKUP_AND_RECOVERY]
-        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+# class APIGatewayLogging(BaseResourceCheck):
+#     def __init__(self):
+#         name = "Ensure RDS instances are configured for at least 3 AZs.  Also add at least one reader node for Aurora clusters."
+#         id = "CKV_IBT_010"
+#         supported_resources = ['aws_api_gateway_method_settings']
+#         # Look at checkov/common/models/enums.py for options
+#         categories = [CheckCategories.BACKUP_AND_RECOVERY]
+#         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-    def scan_resource_conf(self, conf):
-        """
-            Ensure that RDS instances are configured with multi-az and add at least one reader node.
-            To create a Multi-AZ RDS cluster, you must additionally specify the engine, storage_type, allocated_storage, iops and db_cluster_instance_class attributes.
-            https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster
-            https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/multi-az-db-clusters-concepts.html
-        """
-        if self.entity_type == 'aws_api_gateway_method_settings':
-            if 'logging_level' in conf.keys():                
-                log_level = conf["logging_level"]
-                self.name = log_level
-                return CheckResult.PASSED
-            self.name = 'Ensure RDS instances are configured for Multi-AZ. See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster'
-            return CheckResult.FAILED
-scanner = APIGatewayLogging()
+#     def scan_resource_conf(self, conf):
+#         """
+#             Ensure that RDS instances are configured with multi-az and add at least one reader node.
+#             To create a Multi-AZ RDS cluster, you must additionally specify the engine, storage_type, allocated_storage, iops and db_cluster_instance_class attributes.
+#             https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster
+#             https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/multi-az-db-clusters-concepts.html
+#         """
+#         print(self.entity_type)
+#         if self.entity_type == 'aws_api_gateway_method_settings': 
+#             if 'settings' in conf.keys():  
+#                 settings_block = conf['settings']
+#                 if 'logging_level' in settings_block[0]:
+#                     logging_block = settings_block[0]['logging_level']
+#                     self.name = logging_block[0]
+#                     if logging_block in [['OFF'], ['INFO'], ['ERROR'], ['var.log_level']]:
+#                         return CheckResult.PASSED
+
+#             #self.name = 'Ensure RDS instances are configured for Multi-AZ. See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster'
+#             return CheckResult.FAILED
+# scanner = APIGatewayLogging()
