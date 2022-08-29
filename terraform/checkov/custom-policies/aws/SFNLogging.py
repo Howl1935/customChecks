9d93c1ce02080b17e3a535d00debc58c5f54002b
Travis Hawley <travis.hawley@ibotta.com>
2022-08-29 09:38:10 -0700

    SFNLogging tweaks

--- terraform/checkov/custom-policies/aws/SFNLogging.py
+++ terraform/checkov/custom-policies/aws/SFNLogging.py
@@ -3,7 +3,7 @@ from checkov.common.models.enums import CheckResult, CheckCategories
 
 class SFNLogging(BaseResourceCheck):
     def __init__(self):
-        name = "Enable CloudWatch logging if needed for this step function"
+        name = "Enable CloudWatch logging if needed for this step function."
         id = "CKV_IBT_009"
         supported_resources = ['aws_sfn_state_machine']
         # Look at checkov/common/models/enums.py for options
@@ -17,9 +17,14 @@ class SFNLogging(BaseResourceCheck):
         if self.entity_type == 'aws_sfn_state_machine':
             if 'logging_configuration' in conf.keys():
                 logging_block = conf['logging_configuration'][0]
-                if 'include_execution_data' in logging_block:
-                    return CheckResult.PASSED
-                # should we validate?
+                if 'include_execution_data' in logging_block and logging_block['include_execution_data'][0]:
+                    if 'level' in logging_block and logging_block['level'][0] in [['ALL'], ['ERROR'], ['FATAL'], ['OFF']]:
+                        if logging_block['level'][0] == 'OFF':
+                            self.name = "Enable CloudWatch logging if needed for this step function: Please ensure logging is not set to \'OFF\'."
+                            return CheckResult.FAILED
+                        if 'log_destination' in logging_block:
+                            return CheckResult.PASSED
+                    return CheckResult.FAILED
                 return CheckResult.FAILED
             self.name = "Please consider enabling Cloudwatch Logging for your state function: https://docs.aws.amazon.com/step-functions/latest/dg/cw-logs.html and https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sfn_state_machine#logging_configuration-configuration-block"
             return CheckResult.PASSED
